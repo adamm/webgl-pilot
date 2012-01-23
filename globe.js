7f0594299775ad44ec72011366e53665a2f688e0
Adam McDaniel <adam.r.mcdaniel@gmail.com>
2012-01-23 11:48:03 -0700

    Minor code cleanup, add a title header

--- globe.js
+++ globe.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   var container;
   var camera, scene, renderer;
   var sphere1, sphere2;
@@ -6,24 +6,24 @@ $(function(){
 
   // set the scene size
   var WIDTH = window.innerWidth,
-      HEIGHT = window.innerHeight;
+    HEIGHT = window.innerHeight;
 
   // set some camera attributes
   var VIEW_ANGLE = 45,
-      ASPECT = WIDTH / HEIGHT,
-      NEAR = 1,
-      FAR = 10000;
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 1,
+    FAR = 10000;
 
   var layers = [
-    { image: 'gfx/earth.jpg',
-      geometry: [ 200, 50, 50 ],
-      overdraw: true,
-      sphere: {}
+    { image:'gfx/earth.jpg',
+      geometry:[ 200, 50, 50 ],
+      overdraw:true,
+      sphere:{}
     },
-    { image: 'gfx/clouds.png',
-      geometry: [ 220, 50, 50 ],
-      overdraw: false,
-      sphere: {}
+    { image:'gfx/clouds.png',
+      geometry:[ 220, 50, 50 ],
+      overdraw:false,
+      sphere:{}
     }
   ];
 
@@ -32,13 +32,12 @@ $(function(){
 
   function init() {
 
-    container = document.createElement('div');
-    document.body.appendChild(container);
+    container = $('div#container')
 
-    camera = new THREE.PerspectiveCamera(  VIEW_ANGLE,
-                                    ASPECT,
-                                    NEAR,
-                                    FAR  );
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE,
+      ASPECT,
+      NEAR,
+      FAR);
     // the camera starts at 0,0,0 so pull it back
     camera.position.z = 215;
     camera.rotation.z = 1.54;
@@ -46,23 +45,24 @@ $(function(){
 
     // Declare the 3D scene wtih camera
     scene = new THREE.Scene();
+    var texture = THREE.ImageUtils.loadTexture()
     scene.add(camera);
 
-    for ( var i = 0; i < layers.length; i++ ) {
+    for (var i = 0; i < layers.length; i++) {
       var layer = layers[i];
 
       // load the sphere's texture
       var texture = THREE.ImageUtils.loadTexture(
-        layer.image/*, undefined, function() {
-          // This call ensures render() re-runs after the image is loaded.
-          // This isn't necessary if the scene is animated.
-          render();
-      }*/);
+        layer.image, undefined, function() {
+         // This call ensures render() re-runs after the image is loaded.
+         // This isn't necessary if the scene is animated.
+         render();
+         });
 
       // create the sphere's material
       var material = new THREE.MeshBasicMaterial({
-          map: texture,
-          overdraw: layer.overdraw
+        map:texture,
+        overdraw:layer.overdraw
       });
 
       // assign the sphere's geometry
@@ -71,20 +71,20 @@ $(function(){
       // create a new mesh with sphere geometry
       layer.sphere = new THREE.Mesh(geometry, material);
       layer.sphere.doubleSided = true;
-      layer.sphere.rotation.x = -100;
+      layer.sphere.rotation.x = 1;
       scene.add(layer.sphere);
     }
 
     // Create the WebGL Renderer, apply it to the DOM
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({antialias:true});
     renderer.setSize(WIDTH, HEIGHT);
-    container.appendChild(renderer.domElement);
+    container.append(renderer.domElement);
 
     window.addEventListener('resize', onWindowResize, false);
   }
 
   function onWindowResize(event) {
-    WIDTH  = window.innerWidth;
+    WIDTH = window.innerWidth;
     HEIGHT = window.innerHeight;
 
     renderer.setSize(WIDTH, HEIGHT);
